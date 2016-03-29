31eaa30b51079b89a42374e90d8e6f58ca2d0ab4
LenmonLIN <Lenmon@localhost.localdomain>
2016-03-30 07:58:35 +0800

    9.3 create user permission

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from  flask import current_app
 
@@ -11,10 +11,33 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions =db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    @staticmethod
+    def insert_roles():
+        roles={
+            'User':(Permission.FOLLOW |
+                    Permission.COMMIT |
+                    Permission.WRITE_ARTICLES,True),
+            'Moderator': (Permission.FOLLOW|
+                          Permission.COMMIT|
+                          Permission.WRITE_ARTICLES|
+                          Permission.MODERATE_COMMENTS,False),
+            'Administrator':(0xff,False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions=roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 
 class User(UserMixin,db.Model):
@@ -59,6 +82,38 @@ class User(UserMixin,db.Model):
         self.confirmed =True
         db.session.add(self)
         return True
+
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self,permissions):
+        return self.role is not None and \
+               (self.role.permissions & permissions) == permissions
+
+    def is_administrators(self):
+        return self.can(Permission.ADMINISTER)
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self,permissions):
+        return False
+
+    def is_adminstrator(self):
+        return False
+
+login_manager.anonymous_user=AnonymousUser
+
+class Permission:
+    FOLLOW = 0x01
+    COMMIT = 0x02
+    WRITE_ARTICLES=0x04
+    MODERATE_COMMENTS=0x08
+    ADMINISTER=0X80
+
 
 
 @login_manager.user_loader
