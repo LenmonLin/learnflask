6e0b28596811746e57a8f9e0de970fac1cca5926
LenmonLIN <Lenmon@localhost.localdomain>
2016-04-06 11:09:39 +0800

    12.1 many to many database

--- app/models.py
+++ app/models.py
@@ -147,9 +147,30 @@ class User(UserMixin,db.Model):
             except IntegrityError:
                 db.session.rollback()
 
+    followed=db.relationship('Follow',foreign_keys=[Follow.follower_id],backref=db.backref('follower',lazy='joined'),
+                             lazy='dynamic',cascade='all delete-orphan')
+    followers=db.relationship('Follow',
+                              foreign_keys=[Follow.followed_id],backref=db.backref('followed',lazy='joined'),
+                              cascade='all delete-orphan')
+    def follow(self,user):
+        if not self.is_following(user):
+            f = Follow(follower=self,followed=user)
+            db.session.add(f)
 
+    def unfollow(self,user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f :
+            db.session.delete(f)
+    def is_following(self,user):
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+    def is_followed_by(self,user):
+        return self.followers.filter_by(followed_id=user.id).first() is not None
 
-
+class Follow(db.Model):
+    __tablename__='follows'
+    follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True)
+    timestamp = db.Column(db.DateTime,default=datetime.utcnow)
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self,permissions):
